ba1d9c115ae435f93f58081569b1d1b762ec8879
Junya Ogino <j.ogino@kakehashi.life>
2024-09-15 21:18:28 +0900

    見た目をいじった

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,4 @@
 module ApplicationHelper
-  def container_class
-    if controller_name == "pages" && action_name == "welcome"
-      "container-full"
-    else
-      "container"
-    end
-  end
-
   def avatar_url
     "https://i.pravatar.cc/500?img=#{(1..70).to_a.sample}"
   end
